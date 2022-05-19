74584ae625d186fb34a74f8f228e76a2fa37f59c
manojvuppu <manoj.vuppu@gmail.com>
2022-05-19 14:22:57 -0400

    1 file modified, 2 added

---
+++ src/app/cl-back-fn/cl-back-fn.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cl-back-fn',
+  templateUrl: './cl-back-fn.component.html',
+  styleUrls: ['./cl-back-fn.component.css']
+})
+export class ClBackFnComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
