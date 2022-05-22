4ffa7f6e8f8b9d871ca7bd4b45af1435dede7d61
manojvuppu <manoj.vuppu@gmail.com>
2022-05-22 17:14:20 -0400

    1 file modified, 2 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,11 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { ClBackFnComponent } from './cl-back-fn/cl-back-fn.component';
+import { CallBindApplyComponent } from './call-bind-apply/call-bind-apply.component';
 
 @NgModule({
   imports: [BrowserModule, FormsModule],
-  declarations: [AppComponent, HelloComponent, ClBackFnComponent],
+  declarations: [AppComponent, HelloComponent, ClBackFnComponent,CallBindApplyComponent],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
---
+++ src/app/call-bind-apply/call-bind-apply.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-call-bind-apply',
+  templateUrl: './call-bind-apply.component.html',
+  styleUrls: ['./call-bind-apply.component.css']
+})
+export class CallBindApplyComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
