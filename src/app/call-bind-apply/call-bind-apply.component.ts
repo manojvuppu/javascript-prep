a01a58704e5598f9ee9ffe41280fe897daa3f23f
manojvuppu <manoj.vuppu@gmail.com>
2022-05-22 18:05:35 -0400

    2 files modified, 1 added

--- src/app/call-bind-apply/call-bind-apply.component.ts
+++ src/app/call-bind-apply/call-bind-apply.component.ts
@@ -3,13 +3,36 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-call-bind-apply',
   templateUrl: './call-bind-apply.component.html',
-  styleUrls: ['./call-bind-apply.component.css']
+  styleUrls: ['./call-bind-apply.component.css'],
 })
 export class CallBindApplyComponent implements OnInit {
+  poll = {
+    question: 'What is your favourite programming language?',
+    options: ['0: JavaScript', '1: Python', '2: Rust', '3: C++'],
+    answers: new Array(4).fill(0),
+    registerNewAnswer() {
+      return Number(
+        prompt(
+          `${this.question}\n${this.options.join('\n')}\n(write option number)`
+        )
+      );
+      // console.log(test);
+    },
+  };
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
+    // console.log(this.poll.registerNewAnswer());
+    if (this.poll.registerNewAnswer() <= 3) {
+      this.poll.answers[this.poll.registerNewAnswer()]++;
+    }
+    console.log(this.poll);
+
+    // prompt()
   }
 
+  // registerNewAnswer(){
+
+  // }
 }
