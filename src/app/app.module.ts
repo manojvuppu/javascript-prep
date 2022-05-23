b6d5c1bb174218daa3464138ebc709d29a156e83
manojvuppu <manoj.vuppu@gmail.com>
2022-05-23 10:06:49 -0400

    re do

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,11 @@ import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { ClBackFnComponent } from './cl-back-fn/cl-back-fn.component';
 import { CallBindApplyComponent } from './call-bind-apply/call-bind-apply.component';
+import { RestestCallBindComponent } from './restest-call-bind/restest-call-bind.component';
 
 @NgModule({
   imports: [BrowserModule, FormsModule],
-  declarations: [AppComponent, HelloComponent, ClBackFnComponent,CallBindApplyComponent],
+  declarations: [AppComponent, HelloComponent, ClBackFnComponent,CallBindApplyComponent,RestestCallBindComponent],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
---
+++ src/app/restest-call-bind/restest-call-bind.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-restest-call-bind',
+  templateUrl: './restest-call-bind.component.html',
+  styleUrls: ['./restest-call-bind.component.css']
+})
+export class RestestCallBindComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
