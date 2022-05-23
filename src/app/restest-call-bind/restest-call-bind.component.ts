a40da4af152008ac9557affeb3af375d8efd816d
manojvuppu <manoj.vuppu@gmail.com>
2022-05-23 10:49:15 -0400

    Redid Ass

--- src/app/restest-call-bind/restest-call-bind.component.ts
+++ src/app/restest-call-bind/restest-call-bind.component.ts
@@ -3,13 +3,44 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-restest-call-bind',
   templateUrl: './restest-call-bind.component.html',
-  styleUrls: ['./restest-call-bind.component.css']
+  styleUrls: ['./restest-call-bind.component.css'],
 })
 export class RestestCallBindComponent implements OnInit {
+  poll = {
+    question: 'What is your favourite programming language?',
+    options: ['0: JavaScript', '1: Python', '2: Rust', '3: C++'],
+    answers: new Array(4).fill(0),
+    registerNewAnswer() {
+      const answer = Number(
+        prompt(
+          `${this.question}\n${this.options.join('\n')}(Write option number)`
+        )
+      );
+      typeof answer === 'number' &&
+        answer < this.options.length &&
+        this.answers[answer]++;
+      this.displayResults('string');
+    },
+    displayResults(type: string | [] = 'array') {
+      debugger;
+      if (type === 'array') {
+        console.log(`${this.answers}`);
+      } else if (type === 'string') {
+        console.log(`poll results are ${this.answers.join(', ')}`);
+      }
+    },
+  };
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
-  }
+    document
+      .querySelector('.answer')
+      .addEventListener('click', this.poll.registerNewAnswer.bind(this.poll));
 
+    // this.poll.registerNewAnswer();
+    console.log(this.poll);
+
+    this.poll.displayResults.call({ answers: [1, 2, 3] }, 'string');
+  }
 }
